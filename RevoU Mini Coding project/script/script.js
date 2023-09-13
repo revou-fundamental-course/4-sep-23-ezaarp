ebc7ce27ff89e90fac4a38f962b2bd69b07cc58c
ezaarp <141315114+ezaarp@users.noreply.github.com>
2023-09-13 19:44:32 +0700

    Delete RevoU Mini Coding project directory

--- RevoU Mini Coding project/script/script.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-    const sisi = parseFloat(document.getElementById("sisi").value);
-    
-    const luas = sisi * sisi;
-    const keliling = 4 * sisi;
-
-    document.getElementById("luas").textContent = luas;
-    document.getElementById("keliling").textContent = keliling;
-}
